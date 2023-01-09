616c0a6c2145fafb5041f747121a5945bf774b7c
ellliot007 <roshanborkarrb@gmail.com>
2023-01-09 20:40:08 +0530

    Added two endpoints in urls.py and two views with NotImplemented

--- Server/wup_wspc_calculator/calculator/urls.py
+++ Server/wup_wspc_calculator/calculator/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 
-# URL patterns for the calculate_wup and calculate_wspc functions
+# URL patterns for the endpoint1 and endpoint2 functions
 urlpatterns = [
-    path('calculate_wup/', views.calculate_wup, name='calculate_wup'),
-    path('calculate_wspc/', views.calculate_wspc, name='calculate_wspc'),
+    path('endpoint1/', views.endpoint1, name='endpoint1'),
+    path('endpoint2/', views.endpoint2, name='endpoint2'),
 ]
--- Server/wup_wspc_calculator/calculator/views.py
+++ Server/wup_wspc_calculator/calculator/views.py
@@ -1,33 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from django.http import JsonResponse
-from .wup_processing import *
 # Create your views here.
 
-@api_view(['GET'])
-def calculate_wup(request):
-    # Get input values from the request
-    dis_value = request.GET['dis_value']
-    lup_value = request.GET['lup_value']
-    up_value = request.GET['up_value']
-    
-    # Calculate WUP
-    wup_calculator = WupCalculator(up_value, dis_value, lup_value)
-    wup_value = wup_calculator.calculate()
-    
-    # Return the result as a JSON response
-    return JsonResponse({"wup": wup_value})
+#Endpoint1_View
+@api_view(['PUT', 'GET'])
+def endpoint1(request):
+    raise NotImplementedError
 
-@api_view(['GET'])
-def calculate_wspc(request):
-    # Get input values from the request
-    area = request.GET['area']
-    resident_employee_count = request.GET['resident_employee_count']
-    wup_value = request.GET['wup_value']
-    
-    # Calculate WSPC
-    wspc_calculator = WspcCalculator(area, resident_employee_count, wup_value)
-    wspc_value = wspc_calculator.calculate()
-    
-    # Return the result as a JSON response
-    return JsonResponse({"wspc": wspc_value})
+#Endpoint2_View
+@api_view(['PUT', 'GET'])
+def endpoint1(request):
+    raise NotImplementedError
