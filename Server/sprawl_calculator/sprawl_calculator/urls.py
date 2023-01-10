57194bb785df53af1e659d9bab5ada1f0ea8c00e
ellliot007 <roshanborkarrb@gmail.com>
2023-01-10 20:52:48 +0530

    module calculator rename to wup_calculator

--- Server/sprawl_calculator/sprawl_calculator/urls.py
+++ Server/sprawl_calculator/sprawl_calculator/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('calculator.urls'))
+    path('',include('wup_calculator.urls'))
 ]
--- Server/sprawl_calculator/wup_calculator/apps.py
+++ Server/sprawl_calculator/wup_calculator/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CalculatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calculator'
+    name = 'wup_calculator'
