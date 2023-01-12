7a73acd334f221d4d2a440e5a8bb42244ba87676
ellliot007 <roshanborkarrb@gmail.com>
2023-01-13 00:49:53 +0530

    Create Sample model with fields

--- Server/sprawl_calculator/wup_calculator/models.py
+++ Server/sprawl_calculator/wup_calculator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Model(models.Model):
+	id = models.UUIDField() #Id
+	field1 = models.CharField() #Character
+	field2 = models.DateTimeField() #Time
+	status = models.IntegerField() #Status
+
+	def __str__(self):
+        return self.id
