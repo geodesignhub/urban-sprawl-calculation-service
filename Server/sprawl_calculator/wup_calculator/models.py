7605fc9ec060b0b99db1db16f453e042362ccf73
ellliot007 <roshanborkarrb@gmail.com>
2023-01-13 22:40:07 +0530

    field name changed 'WUPCalculationRequestStatus' to 'status'

--- Server/sprawl_calculator/wup_calculator/models.py
+++ Server/sprawl_calculator/wup_calculator/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class WUPCalculation(models.Model):
     uuid = models.UUIDField(max_length=36, min_length=36, format='uuid', default=uuid.uuid4, unique=True)
-    WUPCalculationRequestStatus = models.CharField(
+    Status = models.CharField(
         max_length=255,
         choices=[
             ('Activating', 'Activating'),
