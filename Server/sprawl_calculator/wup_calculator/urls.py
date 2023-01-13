66a74b7d8cf16c5374194c3cf2b57acd0e7a19a8
ellliot007 <roshanborkarrb@gmail.com>
2023-01-13 10:23:21 +0530

    '>' removed at the end of the url path

--- Server/sprawl_calculator/wup_calculator/urls.py
+++ Server/sprawl_calculator/wup_calculator/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 #Url for wup_index_generator and index_calculation_status
 urlpatterns = [
-    path('v1/wup_index>',wup_index_generator, name='wup_index_generator'),
+    path('v1/wup_index',wup_index_generator, name='wup_index_generator'),
     path('v1/wup_index/<uuid:processing_id>',index_calculation_status, name='index_calculation_status'),
 ]
