30df6d49cda7369e7474a46373cf646a2e2caad3
ellliot007 <roshanborkarrb@gmail.com>
2023-01-13 22:44:21 +0530

    Two new fields added

--- Server/sprawl_calculator/wup_calculator/models.py
+++ Server/sprawl_calculator/wup_calculator/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class WUPCalculation(models.Model):
     uuid = models.UUIDField(max_length=36, min_length=36, format='uuid', default=uuid.uuid4, unique=True)
-    Status = models.CharField(
+    status = models.CharField(
         max_length=255,
         choices=[
             ('Activating', 'Activating'),
@@ -15,4 +15,5 @@ class WUPCalculation(models.Model):
         ],
         default='Activating'
     )
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
