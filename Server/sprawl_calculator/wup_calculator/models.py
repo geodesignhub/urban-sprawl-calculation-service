9a066243f223127d54b581c871f7190a34997663
ellliot007 <roshanborkarrb@gmail.com>
2023-01-13 18:52:41 +0530

    Created model with fields id and status

--- Server/sprawl_calculator/wup_calculator/models.py
+++ Server/sprawl_calculator/wup_calculator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class WUPCalculation(models.Model):
+    uuid = models.UUIDField(max_length=36, min_length=36, format='uuid', default=uuid.uuid4, unique=True)
+    WUPCalculationRequestStatus = models.CharField(
+        max_length=255,
+        choices=[
+            ('Activating', 'Activating'),
+            ('Processing', 'Processing'),
+            ('Completed', 'Completed'),
+            ('Rejected', 'Rejected'),
+            ('Error', 'Error')
+        ],
+        default='Activating'
+    )
+    
