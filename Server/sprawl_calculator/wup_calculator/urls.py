594f2a2ca75f74a76e614d15e06980117adc9a08
ellliot007 <roshanborkarrb@gmail.com>
2023-01-13 10:20:26 +0530

    Processing_index changed to processing_id with type uuid

--- Server/sprawl_calculator/wup_calculator/urls.py
+++ Server/sprawl_calculator/wup_calculator/urls.py
@@ -4,5 +4,5 @@ from . import views
 #Url for wup_index_generator and index_calculation_status
 urlpatterns = [
     path('v1/wup_index>',wup_index_generator, name='wup_index_generator'),
-    path('v1/wup_index/<processing_index>',index_calculation_status, name='index_calculation_status'),
+    path('v1/wup_index/<uuid:processing_id>',index_calculation_status, name='index_calculation_status'),
 ]
