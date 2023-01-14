7af9262c3d1391dc4e85fdacc11ebb480e881186
ellliot007 <roshanborkarrb@gmail.com>
2023-01-14 19:27:01 +0530

    new fields are added as per the documentation

--- Server/sprawl_calculator/wup_calculator/data_definitions.py
+++ Server/sprawl_calculator/wup_calculator/data_definitions.py
@@ -5,23 +5,34 @@ from dataclasses import dataclass
 class WUPCalculationRequestActivating:
 	status: str
 	processing_id: str
+	created_at: datetime
+	updated_at: datetime
 
 @dataclass
 class WUPCalculationRequestProcessing:
 	status: str
 	processing_id: str
+	created_at: datetime
+	updated_at: datetime
 
 @dataclass
 class WUPCalculationRequestCompleted:
 	status: str
 	processing_id: str
-
+	result: float
+	created_at: datetime
+	updated_at: datetime
+	
 @dataclass
 class WUPCalculationRequestRejected:
 	status: str
 	processing_id: str
+	created_at: datetime
+	updated_at: datetime
 
 @dataclass
 class WUPCalculationRequestError:
 	status: str
 	processing_id: str
+	created_at: datetime
+	updated_at: datetime
