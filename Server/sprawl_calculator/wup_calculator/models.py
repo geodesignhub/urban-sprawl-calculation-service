9fb2e11cc9c901d28d5927ac36c4f3cb612133cf
ellliot007 <roshanborkarrb@gmail.com>
2023-01-18 11:02:40 +0530

    imports and required parameters added to data_defination and models

--- Server/sprawl_calculator/wup_calculator/models.py
+++ Server/sprawl_calculator/wup_calculator/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from datetime import datetime
 import uuid
 # Create your models here.
 
 class WUPCalculation(models.Model):
     uuid = models.UUIDField(max_length=36, min_length=36, format='uuid', default=uuid.uuid4, unique=True)
+    resident_count_in_boundary = models.IntegerField()
+    employment_count_in_boundary = models.IntegerField()
+    raster_with_build_up_area = models.URLField()
+    raster_no_data_value = models.IntegerField()
+    raster_build_up_value = models.IntegerField()
+    vector_boundary = models.PolygonField()
     status = models.CharField(
         max_length=255,
         choices=[
