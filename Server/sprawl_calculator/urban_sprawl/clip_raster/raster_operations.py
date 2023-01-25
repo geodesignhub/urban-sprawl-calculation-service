48f4382fea6de3d3ae5b4dd6c7f2f4ae1fb36deb
ellliot007 <roshanborkarrb@gmail.com>
2023-01-25 16:40:20 +0530

    Method clip_raster_with_geojson created

--- Server/sprawl_calculator/urban_sprawl/clip_raster/raster_operations.py
+++ Server/sprawl_calculator/urban_sprawl/clip_raster/raster_operations.py
@@ -1,4 +1,21 @@
 import rasterio
+from rasterio.mask import mask
 
-def clip_raster_with_geojson(request):
-	pass
+
+def clip_raster_with_geojson(raster_file, geojson):
+	# load the raster, mask it by the polygon and crop it
+	with rasterio.open(raster_file) as src:
+		out_image, out_transform = mask(src, geojson, crop=True)
+		out_meta = src.meta.copy()
+
+	# save the resulting raster  
+	out_meta.update({"driver": "GTiff",
+		"height": out_image.shape[1],
+	    "width": out_image.shape[2],
+	    "transform": out_transform})
+
+	with rasterio.open(raster_file, "w", **out_meta) as dest:
+	    dest.write(out_image)
+
+	return out_image, out_transform
+
