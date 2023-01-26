de8ae2d404025448b94e988f55e5cb11c298997f
ellliot007 <roshanborkarrb@gmail.com>
2023-01-26 14:43:23 +0530

    Type of raster_file changed to NameTemporaryFile

--- Server/sprawl_calculator/urban_sprawl/clip_raster/raster_operations.py
+++ Server/sprawl_calculator/urban_sprawl/clip_raster/raster_operations.py
@@ -1,8 +1,9 @@
 import rasterio
 from rasterio.mask import mask
+from tempfile import NamedTemporaryFile
 
-
-def clip_raster_with_geojson(raster_file, geojson):
+def clip_raster_with_geojson(raster_file: NamedTemporaryFile, geojson):
+	print(raster_file)
 	# load the raster, mask it by the polygon and crop it
 	with rasterio.open(raster_file) as src:
 		out_image, out_transform = mask(src, geojson, crop=True)
@@ -14,8 +15,8 @@ def clip_raster_with_geojson(raster_file, geojson):
 	    "width": out_image.shape[2],
 	    "transform": out_transform})
 
-	with rasterio.open(raster_file, "w", **out_meta) as dest:
+	output_file = NamedTemporaryFile(suffix='.tif')
+	with rasterio.open(output_file, "w", **out_meta) as dest:
 	    dest.write(out_image)
 
 	return out_image, out_transform
-
