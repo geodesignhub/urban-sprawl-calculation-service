9adf4d3e85f231980d1c43be1a878b0016a94d32
ellliot007 <roshanborkarrb@gmail.com>
2023-02-01 20:23:21 +0530

    Dropped QGIS things and rewrited pseudocode

--- Server/sprawl_calculator/wup_calculator/process_algorithm.py
+++ Server/sprawl_calculator/wup_calculator/process_algorithm.py
@@ -5,19 +5,12 @@ and it is used to perform some action for processing a raster and a vector layer
 
 def processAlgorithm():
 	'''
-	1.Get the path of the input raster layer and output path.
-	2.Copy the input vector layer and add it to the QGIS project.
-	3.Check and add the necessary fields to the vector layer, such as "Dis" and "settlement_area".
-	4.Get the Spatial Index library using the get_si_lib() method.
-	5.Open the input raster layer using GDAL and get its pixel size.
-	6.Get all the features of the vector layer.
-	7.Start a loop through all the features.
-	8.Use the get_si_raster() method to get the clipped raster, result matrix, and clipped matrix for each feature.
-	9.Save the clipped raster for each feature using the save_raster() method.
-	10.Calculate and save the DIS for each feature using the calculate_and_save_dis() method.
-	11.Calculate and save the WDIC for each feature using the calculate_and_save_wdis() method.
-	12.Use the calculate() method to calculate the build-up area and settlement area for each feature.
-	13.Write the vector layer to the output path in GPKG format.
-	14.Set the progress of the processing feedback and return the output path.
+	1.Get the URL for the input raster layer.
+	2.Check and add the necessary fields to the vector layer, such as "Dis" and "settlement_area".
+	3.Run clip_raster_with_geojson() for the downloded raster and the geojson feature.
+	4.Calculate and save the DIS for each feature using the calculate_and_save_dis() method.
+	5.Calculate and save the WDIC for each feature using the calculate_and_save_wdis() method.
+	6.Use the calculate() method to calculate the build-up area and settlement area for each feature.
+	7.Return the results as a JSON object.
 	'''
 	raise NotImplementedError
