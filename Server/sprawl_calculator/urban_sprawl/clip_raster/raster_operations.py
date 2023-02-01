fc1109eb5761a8b8d1218dd423fc344f17afdec6
hrishiballal <hrishikeshballal@yahoo.com>
2023-02-01 12:52:44 +0000

    Update raster_operations.py

--- Server/sprawl_calculator/urban_sprawl/clip_raster/raster_operations.py
+++ Server/sprawl_calculator/urban_sprawl/clip_raster/raster_operations.py
@@ -2,8 +2,8 @@ import rasterio
 from rasterio.mask import mask
 from tempfile import NamedTemporaryFile
 
-def clip_raster_with_geojson(raster_file: NamedTemporaryFile, geojson):
-	print(raster_file)
+def clip_raster_with_geojson(raster_file: NamedTemporaryFile, geojson) -> NamedTemporaryFile:
+	
 	# load the raster, mask it by the polygon and crop it
 	with rasterio.open(raster_file) as src:
 		out_image, out_transform = mask(src, geojson, crop=True)
@@ -12,11 +12,11 @@ def clip_raster_with_geojson(raster_file: NamedTemporaryFile, geojson):
 	# save the resulting raster  
 	out_meta.update({"driver": "GTiff",
 		"height": out_image.shape[1],
-	    "width": out_image.shape[2],
-	    "transform": out_transform})
+		"width": out_image.shape[2],
+		"transform": out_transform})
 
 	output_file = NamedTemporaryFile(suffix='.tif')
 	with rasterio.open(output_file, "w", **out_meta) as dest:
-	    dest.write(out_image)
+		dest.write(out_image)
 
-	return out_image, out_transform
+	return output_file
