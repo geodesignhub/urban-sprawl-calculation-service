ba61a18db716a18db8f8654f55d1afa984543fc9
ellliot007 <roshanborkarrb@gmail.com>
2023-02-02 23:56:00 +0530

    added status code check and url type

--- Server/sprawl_calculator/urban_sprawl/clip_raster/raster_downloader.py
+++ Server/sprawl_calculator/urban_sprawl/clip_raster/raster_downloader.py
@@ -1,8 +1,11 @@
 import requests
 from tempfile import NamedTemporaryFile
 
-def download_raster(url):
+def download_raster(url: str) -> str:
     response = requests.get(url)
-    with NamedTemporaryFile(delete=False, suffix='.tif') as f:
-        f.write(response.content)
-        return f.name
+    if response.status_code == 200:
+        with NamedTemporaryFile(delete=False, suffix='.tif') as f:
+            f.write(response.content)
+            return f.name
+    else:
+        raise Exception("Unable to download raster from URL, status code: {}".format(response.status_code))
