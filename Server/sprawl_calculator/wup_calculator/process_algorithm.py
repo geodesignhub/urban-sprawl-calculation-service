9d82d228ce168cc72534e5341b9d3e27b132e94a
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2023-02-03 13:06:49 +0000

    Update process_algorithm.py

--- Server/sprawl_calculator/wup_calculator/process_algorithm.py
+++ Server/sprawl_calculator/wup_calculator/process_algorithm.py
@@ -5,7 +5,7 @@ and it is used to perform some action for processing a raster and a vector layer
 
 def processAlgorithm():
 	'''
-	1.Get the URL for the input raster layer.
+	1.Get the downloaded file as input raster layer.
 	2.Check and add the necessary fields to the vector layer, such as "Dis" and "settlement_area".
 	3.Run clip_raster_with_geojson() for the downloded raster and the geojson feature.
 	4.Calculate and save the DIS for each feature using the calculate_and_save_dis() method.
