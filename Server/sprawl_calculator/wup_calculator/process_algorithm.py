b7fa9b41c4238da64861d83a49256d6e0e78508d
Hrishikesh Ballal <hballal@gmail.com>
2023-02-05 00:10:07 +0000

    Update process_algorithm.py

--- Server/sprawl_calculator/wup_calculator/process_algorithm.py
+++ Server/sprawl_calculator/wup_calculator/process_algorithm.py
@@ -2,6 +2,8 @@ from urban_sprawl.raster_helper.raster_downloader import download_raster
 from urban_sprawl.raster_helper.raster_operations import clip_raster_with_geojson
 from urban_sprawl.dis.dis_calculator import DisCalculator
 from urban_sprawl.common.common import Common
+from urban_sprawl.raster_helper.data_definitions import GeoJSONFeature
+from .data_definitions import WUPCalculationResult
 import rasterio
 from urban_sprawl.wup_processing import (
     get_si_raster_server,
@@ -19,14 +21,14 @@ The "processAlgorithm" method is a part of the QGIS Processing Framework
 and it is used to perform some action for processing a raster and a vector layer.
 '''
 
-def processAlgorithm(url, geojson):
+def processAlgorithm(url:str, geojson:GeoJSONFeature) -> WUPCalculationResult:
 
 	# 1.Get the downloaded file as input raster layer.
 	raster_file = download_raster(url)
 	raster = gdal.Open(raster_file)
 	# Extract spatial metadata
 	# 2.Check and add the necessary fields to the vector layer, such as "Dis" and "settlement_area".
-	clipped_raster_file = clip_raster_with_geojson(raster_file, geojson)
+	clipped_raster_file = clip_raster_with_geojson(raster_file, geojson.feature)
 	pixel_size = Common.get_pixel_size(raster)
 	si_lib = get_si_lib()
 	clipped_matrix, clipped_raster, result_matrix = get_si_raster_server(	
